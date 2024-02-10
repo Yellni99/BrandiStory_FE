504f24c1dacf2f2632d766bbc238efaa45aa95bd
Yellni99 <148676218+Yellni99@users.noreply.github.com>
2024-02-11 00:43:01 +0900

    기초틀 수정
    기초틀수정

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartCheck } from "react-bootstrap-icons";
 import "./ProductDetail.css";
@@ -6,6 +6,22 @@ import "./ProductDetail.css";
 const ProductDetail = () => {
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [viewOptions, setViewOptions] = useState(false);
+
+  // 이펙트를 사용하여 백엔드에서 금액 값 받아오기
+  useEffect(() => {
+    // 가격 값 API 받아오기
+    setPrice();
+  }, [color, size, quantity]);
+
+  const handleRemoveButton = () => {
+    setColor("");
+    setSize("");
+    setQuantity(1);
+    setViewOptions(false);
+  };
 
   return (
     <div className="product-box">
@@ -18,7 +34,9 @@ const ProductDetail = () => {
         <div className="product">상품명</div>
         <div className="price">가격</div>
         <div className="detail">
-          <div className="pay-post1">빠른페이》 결제시</div>
+          <div className="pay-post1">
+            빠른페이》 <span style={{ color: "gray" }}>결제시</span>
+          </div>
           <div className="pay-post2">1%적립 (200원)</div>
           <div className="pay-post3">
             ★★★★★{" "}
@@ -36,18 +54,25 @@ const ProductDetail = () => {
           <select
             className="colorSelect"
             value={color}
-            onChange={(e) => setColor(e.target.value)}
+            onChange={(e) => {
+              setColor(e.target.value);
+              setSize("");
+            }}
           >
             <option value="">색상을 선택하세요.</option>
-            <option value="white">화이트</option>
-            <option value="gray">그레이</option>
-            <option value="black">블랙</option>
+            <option value="화이트">화이트</option>
+            <option value="그레이">그레이</option>
+            <option value="블랙">블랙</option>
           </select>
           <br />
           <select
             className="sizeSelect"
             value={size}
-            onChange={(e) => setSize(e.target.value)}
+            onChange={(e) => {
+              setSize(e.target.value);
+              setViewOptions(true);
+            }}
+            disabled={!color}
           >
             <option value="">사이즈를 선택하세요.</option>
             <option value="S">S</option>
@@ -56,6 +81,25 @@ const ProductDetail = () => {
           </select>
         </div>
         <br />
+        {viewOptions && (
+          <div className="option-box">
+            <p>
+              {color}/{size}
+            </p>
+            <p>
+              <input
+                type="number"
+                value={quantity}
+                onClick={(e) => setQuantity(parseInt(e.target.value))}
+                min={1}
+              />
+            </p>
+            <p>{price}원</p>
+            <button className="remove-button" onClick={handleRemoveButton}>
+              X
+            </button>
+          </div>
+        )}
         <div className="total-price">
           <span>총금액</span>
           <span>가격 0원</span>
