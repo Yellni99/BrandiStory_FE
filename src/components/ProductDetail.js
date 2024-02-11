d1f62a202d8b13b1e3cd3ead59c385a49b14cca7
Yellni99 <148676218+Yellni99@users.noreply.github.com>
2024-02-11 12:21:01 +0900

    틀잡기 완성
    틀잡기 완성

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,20 +1,150 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Cart } from "react-bootstrap-icons";
+import { Heart } from "react-bootstrap-icons";
 import "./ProductDetail.css";
+
 const ProductDetail = () => {
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [viewOptions, setViewOptions] = useState(false);
+
+  // 이펙트를 사용하여 백엔드에서 금액 값 받아오기
+  useEffect(() => {
+    // 가격 값 API 받아오기
+    setPrice();
+  }, [color, size, quantity]);
+
+  const handleRemoveButton = () => {
+    setColor("");
+    setSize("");
+    setQuantity(1);
+    setViewOptions(false);
+  };
+
+  const handleIncrement = () => {
+    setQuantity((prevQantity) => prevQantity + 1);
+  };
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity((prevQantity) => prevQantity - 1);
+    }
+  };
+
+  const handleColorChange = (e) => {
+    setColor(e.target.value);
+    setSize("");
+  };
+
+  const handleSizeChange = (e) => {
+    setSize(e.target.value);
+    setViewOptions(true);
+  };
+
   return (
     <div className="product-box">
       <div className="left-section">
         <div className="main-image"></div>
-        <div className="detailed-images"></div>
+        <div className="detailed-image">
+          <div className="detailed-image1"></div>
+          <div className="detailed-image2"></div>
+          <div className="detailed-image3"></div>
+        </div>
       </div>
       <div className="right-section">
         <div className="company">업체명</div>
         <div className="product">상품명</div>
         <div className="price">가격</div>
+        <div className="detail">
+          <div className="pay-post1">
+            빠른페이》 <span style={{ color: "gray" }}>결제시</span>
+          </div>
+          <div className="pay-post2">1%적립 (200원)</div>
+          <div className="pay-post3">
+            ★★★★★{" "}
+            <span style={{ textDecoration: "underline" }}>500개 리뷰보기</span>{" "}
+            | 520개 구매중
+          </div>
+        </div>
+        <div className="delivery">
+          <span className="delivery-1">배송정보</span>
+          <span className="delivery-2">내일(수) 도착 예정</span>
+          <span className="delivery-3">무료배송</span>
+        </div>
 
-        <div className="pay-post1">빠른페이》 결제시</div>
-        <div className="pay-post2">1%적립 (200원)</div>
-        <div className="pay-post3">★★★★★ 500개 리뷰보기 | 520개 구매중</div>
+        <div>
+          <select
+            className="colorSelect"
+            value={color}
+            onChange={handleColorChange}
+          >
+            <option value="">색상을 선택하세요.</option>
+            <option value="화이트">화이트</option>
+            <option value="그레이">그레이</option>
+            <option value="블랙">블랙</option>
+          </select>
+          <br />
+          <select
+            className="sizeSelect"
+            value={size}
+            onChange={handleSizeChange}
+            disabled={!color}
+          >
+            <option value="">사이즈를 선택하세요.</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+          </select>
+        </div>
+        <br />
+        {viewOptions && (
+          <div className="option-box">
+            <div className="options-1">
+              <div>
+                {color} / {size}
+                <div className="standard-delivery">일반배송</div>
+              </div>
+              <div className="quantity-option">
+                <button
+                  onClick={handleDecrement}
+                  style={{ border: "1px solid gray", background: "white" }}
+                >
+                  -
+                </button>
+                <input className="quantity-Check" value={quantity} min={1} />
+                <button
+                  onClick={handleIncrement}
+                  style={{ border: "1px solid gray", background: "white" }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="options-2">
+              <div className="total">{price}원</div>
+              <button className="remove-button" onClick={handleRemoveButton}>
+                X
+              </button>
+            </div>
+          </div>
+        )}
+        <div className="total-price">
+          <span>총 상품 금액</span>
+          <span>
+            {"     "}
+            {price}원
+          </span>
+        </div>
+        <button className="buyNow">바로 구매</button>
+        <button className="naverPay">네이버페이 구매</button>
+        <button className="addToCart">
+          <Cart />
+        </button>
+        <button className="wish">
+          <Heart />
+        </button>
       </div>
     </div>
   );
