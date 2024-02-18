ec21535da719f81f37a4202607b3e6cfaaa0d56c
hunhee kim <kshkhh4015@gmail.com>
2024-02-18 11:43:29 +0900

    # 경로 수정 및 router 수정

--- src/App.js
+++ src/App.js
@@ -1,103 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Main from "./pages/mainPage/Main";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import ChartMenu from "./components/ChartMenu/ChartMenu";
-import ProductDetail from "./components/ProductDetail";
-import ProductOrder from "./components/Order/ProductOrder";
-import ShoppingBasket from "./components/Basket/ShoppingBasket";
-import MyPage from "./components/MyPage/myPage"; // 경로 수정
-import Cs from "./components/MyPage/cs";
-import Faq from "./components/MyPage/faq";
+import ProductDetail from "./pages/productDetail/ProductDetail";
+import HeaderLayout from "./pages/HeaderLayout";
+import ProductOrder from "./pages/Order/ProductOrder";
+import ShoppingBasket from "./pages/Basket/ShoppingBasket";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HeaderLayout />,
+        children: [
+            { index: true, element: <Main /> },
+            { path: "basket", element: <ShoppingBasket /> },
+            { path: "order", element: <ProductOrder /> },
+            { path: "detail", element: <ProductDetail /> },
+            { path: "order", element: <ProductOrder /> },
 
+        ],
+    },
+]);
 
-
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <Main />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/ProductDetail"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <ProductDetail />
-            </div>
-          }
-        />
-        <Route
-          path="/order"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <ProductOrder />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/basket"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <ShoppingBasket />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/myPage"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <MyPage />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/faq"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <Faq />
-              <Footer />
-            </div>
-          }
-        />
-        <Route
-          path="/cs"
-          element={
-            <div>
-              <Header />
-              <ChartMenu />
-              <Cs />
-              <Footer />
-            </div>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+}
 
 export default App;
