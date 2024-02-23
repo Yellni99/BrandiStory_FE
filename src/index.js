818299e20c7caae75bd5f489f3d8f97eb27eb45c
Bhangdongho <bhang97@naver.com>
2024-02-23 20:26:13 +0900

    마지막 수정

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthProvider } from "./AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
